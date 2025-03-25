bfbcf7cea33431cf3538eacda66b3f73390caa1b
Joel Seger <joel.allarp@gmail.com>
2025-03-25 14:16:51 +0100

    food

--- src/Joel.java
+++ src/Joel.java
@@ -2,4 +2,8 @@ public class Joel {
     public void hello() {
         System.out.println("Hello! From Joel");
     }
+
+    public void food() {
+        System.out.println("Joel likes KEBAB!");
+    }
 }
