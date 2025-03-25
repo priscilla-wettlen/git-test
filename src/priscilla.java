51ce3735c5ff048be8a612377e58caccc6634d2e
Priscilla Silva <priscilla.sg91@gmail.com>
2025-03-25 14:18:53 +0100

    added food function

--- src/priscilla.java
+++ src/priscilla.java
@@ -2,4 +2,7 @@ public class priscilla {
     public void hello(){
         System.out.println("Hi, I'm Priscilla");
     }
+    public void food(){
+        System.out.println("Min favorit mat är pizza ");
+    }
 }
